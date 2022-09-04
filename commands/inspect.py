d21e531dd3307d1ab193d609bedf5aa20a258ebd
Relanit <stulovmaxim3@gmail.com>
2022-09-04 17:02:48 +0300

    bugfix

--- commands/inspect.py
+++ commands/inspect.py
@@ -82,7 +82,7 @@ class Inspect(commands.Cog):
                 await ctx.send(f'/timeout {message.author.name} 10 {reason}')
                 if message.channel.name in self.bot.streams:
                     await db.inspect.update_one({'channel': message.channel.name},
-                                                {'$inc': {'stats': {message.author.name: 1}}})
+                                                {'$inc': {f'stats.{message.author.name}': 1}})
             elif not handle:
                 i = self.warned_users[message.channel.name][message.author.name]
                 timeout = self.timeouts[message.channel.name][i]
@@ -96,7 +96,7 @@ class Inspect(commands.Cog):
                 await message.channel.send(f'/timeout {message.author.name} {timeout} {reason}')
                 if message.channel.name in self.bot.streams:
                     await db.inspect.update_one({'channel': message.channel.name},
-                                                {'$inc': {'stats': {message.author.name: 1}}})
+                                                {'$inc': {f'stats.{message.author.name}': 1}})
 
     @commands.command(
         name='inspect',

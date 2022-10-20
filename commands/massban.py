aa44312bf11f89138de9129251f2359e85fed74d
Relanit <stulovmaxim3@gmail.com>
2022-10-20 20:03:33 +0300

    refactoring + !mb now can auto search banphrase

--- commands/massban.py
+++ commands/massban.py
@@ -4,7 +4,24 @@ import time
 
 from twitchio.ext import commands
 
-reason = 'Сообщение, содержащее запрещённую фразу (от ModBoty). Начато %s'
+reason = 'Сообщение, содержащее запрещённую фразу: "%s" (от ModBoty). Начато %s'
+
+
+def get_substring_counts(strings):
+    def substrings(s1, s2):
+        final = [s1[i:b + 1] for i in range(len(s1)) for b in range(len(s1))]
+        return [i for i in final if i in s1 and i in s2 and len(i) > 2]
+
+    substring_counts = {}
+    for i in range(0, len(strings)):
+        for j in range(i + 1, len(strings)):
+            string1 = strings[i]
+            string2 = strings[j]
+            matches = substrings(string1, string2)
+            for match in matches:
+                substring_counts[match] = substring_counts.get(match, 0) + 1
+
+    return substring_counts
 
 
 class MassBan(commands.Cog):
@@ -12,13 +29,11 @@ class MassBan(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.message_history = {}
-        self.first_senders = {}
         self.ban_phrases = {}
         self.queue = {}
 
         for channel in os.getenv('CHANNELS').split('&'):
             self.message_history[channel] = []
-            self.first_senders[channel] = []
 
     @commands.Cog.event()
     async def event_message(self, message):
@@ -41,47 +56,33 @@ class MassBan(commands.Cog):
                     await ctx.reply('Остановлено')
             return
 
-        self.message_history[message.channel.name].append({'author': message.author.name, 'content': message.content})
+        self.message_history[message.channel.name].append({'author': message.author.name,
+                                                           'content': message.content,
+                                                           'first-msg': bool(int(message.tags['first-msg'])),
+                                                           'time': time.time()})
         if len(self.message_history[message.channel.name]) >= 50:
             del self.message_history[message.channel.name][0]
-
-        for msg in self.first_senders[message.channel.name].copy():
-            if time.time() - msg['time'] > 15:
-                del self.first_senders[message.channel.name][0]
-            else:
-                break
-
-        if message.tags['first-msg'] == '1':
-            self.first_senders[message.channel.name].append({'time': time.time(), 'author': message.author.name})
-            if self.ban_phrases.get(message.channel.name) == '':
-                self.queue[message.channel.name].append(message.author.name)
 
         if self.ban_phrases.get(message.channel.name):
             if self.ban_phrases[message.channel.name] in message.content.lower():
                 self.queue[message.channel.name].append(message.author.name)
 
-
     @commands.command(
         name='mb',
-        aliases=['mt', 'm', 'mbf'],
+        aliases=['mt', 'm'],
         cooldown={'per': 0, 'gen': 60},
-        description='Бан/мут пользователей, написавших сообщение с указанной фразой или первое сообщение в чате. Полное описание - https://vk.cc/chCfLq '
+        description='Бан/мут пользователей, написавших сообщение с указанной фразой. Полное описание - https://vk.cc/chCfLq '
     )
     async def mass_ban(self, ctx):
         if not ctx.channel.bot_is_mod:
             await ctx.reply('Боту необходима модерка для работы этой команды')
             return
 
-        content = ctx.content.lower()
+        content = ban_phrase = ctx.content.lower()
 
-        if not content and ctx.command_alias != 'mbf':
-            if ctx.command_alias == 'mb':
-                await ctx.send('Введите банфразу')
-            else:
-                await ctx.send('Введите время и мутфразу')
+        if not content and ctx.command_alias in ('mt', 'm'):
+            await ctx.send('Введите время и мутфразу')
             return
-
-        ban_phrase = content if ctx.command_alias != 'mbf' else ''
 
         if ctx.command_alias in ('mt', 'm'):
             content_split = content.split(' ', 1)
@@ -96,32 +97,81 @@ class MassBan(commands.Cog):
                 ban_phrase = content_split[1]
             except ValueError:
                 timeout = 300
-            text = f'/timeout %s {timeout} {reason % ctx.author.name}'
+            text = f'/timeout %s {timeout} {reason % (ban_phrase, ctx.author.name)}'
         else:
-            text = f'/ban %s {reason % ctx.author.name}'
+            text = f'/ban %s {reason % (ban_phrase, ctx.author.name)}'
 
-        while ctx.limited:
-            await asyncio.sleep(0.1)
-        await ctx.reply('Запущено')
-
-        start = time.time()
-        self.ban_phrases[ctx.channel.name] = ban_phrase
         self.queue[ctx.channel.name] = []
         banned_users = []
+        reply = 'Запущено'
 
-        if ban_phrase:
-            for message in self.message_history[ctx.channel.name].copy():
-                if ban_phrase in message['content'].lower() and message['author'] not in banned_users:
+        if not ban_phrase:
+            first_messages = [message for message in self.message_history[ctx.channel.name].copy() if message['first-msg']]
+            if not first_messages:
+                self.ban_phrases.pop(ctx.channel.name, None)
+                self.queue.pop(ctx.channel.name, None)
+                await ctx.reply('Сообщений от новых пользователей не найдено')
+                return
+
+            if len(first_messages) > 1:
+                first_senders_copy = first_messages.copy()
+                for i, message in enumerate(first_senders_copy):
+                    if message != first_senders_copy[-1]:
+                        if i == 0:
+                            if first_senders_copy[i+1]['time'] - message['time'] > 1:
+                                first_messages.remove(message)
+                        elif message['time'] - first_senders_copy[i-1]['time'] > 1 and first_senders_copy[i+1]['time'] - message['time'] > 1:
+                            first_messages.remove(message)
+                    elif message['time'] - first_senders_copy[i-1]['time'] > 1:
+                        first_messages.remove(message)
+                        break
+
+            substring_counts = get_substring_counts([message['content'].lower() for message in first_messages])
+            sorted_sub = sorted(substring_counts.items(), key=lambda x: x[1], reverse=True)
+
+            ban_phrase, count = sorted_sub[0] if sorted_sub else ('', 0)
+            for substring in sorted_sub:
+                if substring[1] == count and len(substring[0]) > len(ban_phrase):
+                    ban_phrase = substring[0]
+                elif substring[1] < count:
+                    break
+
+            substring_counts = get_substring_counts(['asd'] * len(first_messages))
+            sorted_sub = sorted(substring_counts.items(), key=lambda x: x[1], reverse=True)
+            found = count > sorted_sub[0][1] / 100 * 60 if count else False
+
+            if not found:
+                while ctx.limited:
+                    await asyncio.sleep(0.1)
+                await ctx.reply('Запущено, банфраза не найдена, будут забанены последние новые пользователи')
+                text = f'/ban %s {reason % ("Первое сообщение в чате", ctx.author.name)}'
+
+                for message in first_messages:
                     while ctx.limited:
                         await asyncio.sleep(0.1)
-                    if ctx.channel.name in self.ban_phrases:
+                    if ctx.channel.name in self.queue:
                         await ctx.send(text % message['author'])
                         banned_users.append(message['author'])
                         await asyncio.sleep(0.3)
                     else:
                         return
-        else:
-            for message in self.first_senders[ctx.channel.name].copy():
+                self.ban_phrases.pop(ctx.channel.name, None)
+                self.queue.pop(ctx.channel.name, None)
+                return
+
+            ban_phrase = ban_phrase[:20]
+            text = f'/ban %s {reason % (ban_phrase, ctx.author.name)}'
+            reply = 'Запущено, банфраза найдена'
+
+        while ctx.limited:
+            await asyncio.sleep(0.1)
+        await ctx.reply(reply)
+
+        start = time.time()
+        self.ban_phrases[ctx.channel.name] = ban_phrase
+
+        for message in self.message_history[ctx.channel.name].copy():
+            if ban_phrase in message['content'].lower() and message['author'] not in banned_users:
                 while ctx.limited:
                     await asyncio.sleep(0.1)
                 if ctx.channel.name in self.ban_phrases:

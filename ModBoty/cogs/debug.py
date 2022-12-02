5f13d82094592f9a3feead7207a21169474bbac3
Relanit <stulovmaxim3@gmail.com>
2022-12-02 20:30:56 +0300

    update debug

--- ModBoty/cogs/debug.py
+++ ModBoty/cogs/debug.py
@@ -1,9 +1,33 @@
+from twitchio import Message
 from twitchio.ext.commands import Cog, command, Context
 
 
 class Debug(Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @Cog.event()
+    async def event_message(self, message: Message):
+        if message.echo:
+            return
+
+        if message.content.startswith(self.bot.prefix) and message.author.name == self.bot.admin:
+            content = message.content.lstrip(self.bot.prefix)
+            if not content:
+                return
+
+            alias = content.split(maxsplit=1)[0].lower()
+            if alias == "mesdebug":
+                try:
+                    content = content.split(maxsplit=1)[1].replace("\\n", "\n")
+                    if "await" in content or "\n" in content:
+                        exec(("async def __ex(self, message): " + "".join(f"\n {l}" for l in content.split("\n"))))
+                        result = await locals()["__ex"](self, message)
+                    else:
+                        result = eval(content)
+                except Exception as e:
+                    result = repr(e)
+                await message.channel.send(f"@{message.author.name} {result}")
 
     @command(name="debug", flags=["admin"])
     async def debug(self, ctx: Context):
